ad8d9036e15cb938e8557ffa204a0cb9f8775cec
Sergey Bogolepov <ace.is.not.my.card@gmail.com>
2018-04-08 15:25:24 +0700

    HW6

--- src/Language.ml
+++ src/Language.ml
@@ -6,30 +6,47 @@ open GT
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap
 open Combinators
+
+open List
                          
 (* States *)
 module State =
   struct
                                                                 
     (* State: global state, local state, scope variables *)
-    type t = {g : string -> int; l : string -> int; scope : string list}
+    type t = {
+      g : string -> int; 
+      l : string -> int; 
+      scope : string list
+    }
+
+    let fail x = 
+      failwith (Printf.sprintf "Undefined variable %s" x)
 
     (* Empty state *)
-    let empty = failwith "Not implemented"
+    let empty = 
+      { g = fail; l = fail; scope = [] }
 
     (* Update: non-destructively "modifies" the state s by binding the variable x 
        to value v and returns the new state w.r.t. a scope
     *)
-    let update x v s = failwith "Not implemented"
+    let update x v s = 
+      let upd f y = if x = y then v else f y in
+      if List.mem x s.scope 
+      then { s with l = upd s.l }
+      else { s with g = upd s.g }
                                 
     (* Evals a variable in a state w.r.t. a scope *)
-    let eval s x = failwith "Not implemented" 
+    let eval s x = 
+      (if List.mem x s.scope then s.l else s.g) x
 
     (* Creates a new scope, based on a given state *)
-    let enter st xs = failwith "Not implemented"
+    let enter st xs = 
+      { g = st.g; l = fail; scope = xs }
 
     (* Drops a scope *)
-    let leave st st' = failwith "Not implemented"
+    let leave st st' = 
+      { st with g = st'.g }
 
   end
     
@@ -52,6 +69,25 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
+    let to_func op =
+      let bti   = function true -> 1 | _ -> 0 in
+      let itb b = b <> 0 in
+      let (|>) f g   = fun x y -> f (g x y) in
+      match op with
+      | "+"  -> (+)
+      | "-"  -> (-)
+      | "*"  -> ( * )
+      | "/"  -> (/)
+      | "%"  -> (mod)
+      | "<"  -> bti |> (< )
+      | "<=" -> bti |> (<=)
+      | ">"  -> bti |> (> )
+      | ">=" -> bti |> (>=)
+      | "==" -> bti |> (= )
+      | "!=" -> bti |> (<>)
+      | "&&" -> fun x y -> bti (itb x && itb y)
+      | "!!" -> fun x y -> bti (itb x || itb y)
+      | _    -> failwith (Printf.sprintf "Unknown binary operator %s" op)    
       
     (* Expression evaluator
 
@@ -60,7 +96,10 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)                                                       
-    let eval st expr = failwith "Not implemented"      
+    let rec eval st = function
+      | Const n -> n
+      | Var   x -> State.eval st x
+      | Binop (op, x, y) -> to_func op (eval st x) (eval st y)     
 
     (* Expression parser. You can use the following terminals:
 
@@ -69,7 +108,26 @@ module Expr =
                                                                                                                   
     *)
     ostap (                                      
-      parse: empty {failwith "Not implemented"}
+      parse:
+          !(Ostap.Util.expr 
+                  (fun x -> x)
+            (Array.map (fun (a, s) -> a, 
+                                List.map  (fun s -> ostap(- $(s)), (fun x y -> Binop (s, x, y))) s
+                              ) 
+                    [|                
+                    `Lefta, ["!!"];
+                    `Lefta, ["&&"];
+                    `Nona , ["=="; "!="; "<="; "<"; ">="; ">"];
+                    `Lefta, ["+" ; "-"];
+                    `Lefta, ["*" ; "/"; "%"];
+                    |] 
+            )
+            primary);
+      
+      primary:
+          n:DECIMAL {Const n}
+        | x:IDENT   {Var x}
+        | -"(" parse -")"
     )
     
   end
@@ -104,11 +162,110 @@ module Stmt =
 
        which returns a list of formal parameters, local variables, and a body for given definition
     *)
-    let eval env ((st, i, o) as conf) stmt = failwith "Not implemented"
-                                
+     let rec eval env ((state, i, o) as conf) = function
+      | Read var -> (
+        match i with 
+        | value::i' -> (State.update var value state, i', o) 
+        | _ -> failwith "Unexpected end of input"
+      )
+      | Write e -> (
+        state, i, o @ [Expr.eval state e]
+      )
+      | Assign (var, e) -> (
+        State.update var (Expr.eval state e) state, i, o
+      )
+      | Seq (s1, s2) -> eval env (eval env conf s1) s2
+      | Skip -> conf
+      | If (cond, truStmt, flsStmt) -> (
+        match (Expr.eval state cond) with
+        | 0 -> eval env conf flsStmt
+        | _ -> eval env conf truStmt  
+      )
+      | While  (cond, body) -> (
+        let rec whileEval cond body ((st, i, o) as conf) = 
+          match Expr.eval st cond with 
+          | 0 -> conf
+          | _ -> whileEval cond body (eval env conf body)
+        in
+        whileEval cond body conf
+      )
+      | Repeat (body, cond) -> (
+        let rec repeatEval ((st, i, o) as conf) =
+          match Expr.eval st cond with 
+          | 0 -> repeatEval (eval env conf body)
+          | _ -> conf
+        in
+        repeatEval (eval env conf body)
+      )
+      | Call (funcName, argExprs) ->
+        let params, locals, body = env#definition funcName in
+        let args = List.map (Expr.eval state) argExprs in
+        let procScope = State.enter state (locals @ params) in 
+        let updatedState = List.fold_left2 
+                  (fun acc param arg -> State.update param arg acc)
+                  procScope params args
+        in
+        let (state', i', o') = eval env (updatedState, i, o) body in
+        let st' = State.leave state state' in
+        (st', i', o')
+
+    let rec parseElifChain elseStmt = function
+      | (cond, body)::tail ->
+        If (cond, body, parseElifChain elseStmt tail)
+      | [] -> match elseStmt with 
+        | None -> Skip
+        | Some body -> body
+
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented"}
+      parse:
+        s:stmt ";" ss:parse {Seq (s, ss)}
+      | stmt;
+
+      stmt:
+        "read" "(" x:IDENT ")"          {Read x}
+      | "write" "(" e:!(Expr.parse) ")" {Write e}
+      | x:IDENT ":=" e:!(Expr.parse)    {Assign (x, e)}
+      | parseSkip
+      | parseIf
+      | parseWhile
+      | parseRepeat
+      | parseCall
+      | parseFor;
+
+      parseCall:
+        funcName:IDENT "(" args: !(Expr.parse)* ")"
+        { Call(funcName, args)};
+
+
+      parseSkip:
+        %"skip" 
+        { Skip };
+
+      parseIf:
+        %"if" cond:!(Expr.parse)
+        %"then" truStmt:parse
+        elifStmts:(%"elif"!(Expr.parse) %"then" parse)*
+        elseStmt:(%"else" parse)?
+        %"fi"
+        { If (cond, truStmt, (parseElifChain elseStmt elifStmts)) };
+
+      parseWhile:
+        %"while" cond:!(Expr.parse)
+        %"do" body:parse
+        %"od" 
+        { While (cond, body) };
+
+      parseRepeat:
+        %"repeat" body:parse
+        %"until" cond:!(Expr.parse)
+        { Repeat (body, cond) };
+      
+      parseFor:
+        %"for" init:parse "," cond:!(Expr.parse) "," upd:parse 
+        %"do" body:parse
+        %"od"
+        { Seq (init, While (cond, Seq (body, upd))) }
     )
       
   end
@@ -121,7 +278,19 @@ module Definition =
     type t = string * (string list * string list * Stmt.t)
 
     ostap (
-      parse: empty {failwith "Not implemented"}
+      argument:IDENT;
+      parse: 
+        %"fun" funcName:IDENT "(" args: !(Util.list0 argument) ")"
+        locals: (%"local" !(Util.list argument))?
+        "{" body: !(Stmt.parse) "}" 
+        { 
+          (funcName, 
+          (args, 
+          (match locals with 
+            | None -> [] 
+            | Some list -> list),
+          body))
+        }
     )
 
   end
@@ -137,7 +306,24 @@ type t = Definition.t list * Stmt.t
 
    Takes a program and its input stream, and returns the output stream
 *)
-let eval (defs, body) i = failwith "Not implemented"
+let eval (defs, body) i = 
+  let module FuncDefs = Map.Make(String) in
+  let definitions = 
+    fold_left (fun m ((name, _) as def) -> FuncDefs.add name def m) FuncDefs.empty defs 
+  in
+  let env = 
+    (object 
+      method definition name = 
+        snd (FuncDefs.find name definitions) 
+    end) 
+  in
+  let _, _, o = Stmt.eval env (State.empty, i, []) body in
+  o  
+
                                    
 (* Top-level parser *)
-let parse = failwith "Not implemented"
+let parse = ostap (
+  defs: !(Definition.parse)*
+  body: !(Stmt.parse)
+  { (defs, body) }
+)
--- src/SM.ml
+++ src/SM.ml
@@ -31,7 +31,64 @@ type config = (prg * State.t) list * int list * Stmt.config
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
 *)                         
-let eval env ((cstack, stack, ((st, i, o) as c)) as conf) = failwith "Not implemented"
+let rec eval env ((cstack, stack, ((st, i, o) as c)) as conf) = function
+| [] -> conf
+| insn::prgTail -> match insn with
+      | BINOP op -> 
+        let (y::x::stackTail) = stack in 
+        eval env (cstack, (Expr.to_func op x y)::stackTail, c) prgTail
+
+      | READ -> 
+        let (z::i') = i in 
+        eval env (cstack, z::stack, (st, i', o)) prgTail
+      
+      | WRITE -> 
+        let (z::stackTail) = stack in 
+        eval env (cstack, stackTail, (st, i, o @ [z])) prgTail
+
+      | CONST i -> 
+        eval env (cstack, i::stack, c) prgTail
+
+      | LD x -> 
+        eval env (cstack, (State.eval st x)::stack, c) prgTail
+
+      | ST x -> 
+        let (z::stackTail) = stack in 
+        eval env (cstack, stackTail, (State.update x z st, i, o)) prgTail
+
+      | LABEL l -> 
+        eval env conf prgTail
+
+      | JMP label -> 
+        eval env conf (env#labeled label)
+
+      | CJMP (cond, label) ->
+        let (top::stackTail) = stack in
+        let prg = (if (cond = "z" && top = 0) || (cond = "nz" && top != 0) 
+                  then env#labeled label
+                  else prgTail)
+        in
+        eval env (cstack, stackTail, c) prg 
+      
+      | BEGIN (args, locals) -> 
+        let rec init_args state = function 
+          | a::args, z::stack -> 
+            let state', stack' = init_args state (args, stack) in 
+            State.update a z state', stack'
+          | [], stack -> state, stack
+        in 
+        let st', stack' = init_args (State.enter st @@ args @ locals) (args, stack) in
+        eval env (cstack, stack', (st', i, o)) prgTail
+      
+      | END ->
+        (match conf with
+        | (program, s)::cstack', stack, (s', i, o) ->
+          eval env (cstack', stack, (State.leave s s', i, o)) program
+        | _ -> conf)
+      
+      | CALL f -> 
+        let cstack, stack, ((s, i, o) as conf) = conf in
+        eval env ((prgTail, s)::cstack, stack, conf) @@ env#labeled f
 
 (* Top-level evaluation
 
@@ -56,4 +113,96 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile (defs, p) = failwith "Not implemented"
+let labelGenerator = object
+  val mutable nextLabel = 0
+
+  method generate = 
+    nextLabel <- nextLabel + 1;
+    "L" ^ string_of_int nextLabel
+
+  method generateNamed name = 
+    nextLabel <- nextLabel + 1;
+    name ^ "_" ^ string_of_int nextLabel
+  end
+
+let rec expr = function
+  | Expr.Var x -> 
+    [LD x]
+
+  | Expr.Const n -> 
+    [CONST n]
+
+  | Expr.Binop (op, x, y) -> 
+    expr x @ 
+    expr y @ 
+    [BINOP op]
+
+let rec compile' = function
+  | Stmt.Seq (s1, s2) -> 
+    compile' s1 @ 
+    compile' s2
+
+  | Stmt.Read x -> 
+    [
+      READ; 
+      ST x
+    ]
+
+  | Stmt.Write e -> 
+    expr e @ 
+    [WRITE]
+
+  | Stmt.Assign (x, e) -> 
+    expr e @ 
+    [ST x]
+
+  | Stmt.If (cond, trueBranch, elseBranch) -> 
+    let endLabel = labelGenerator#generateNamed "if_end" in
+    let elseBranchLabel = labelGenerator#generateNamed "else" in
+    expr cond @ 
+    [CJMP ("z", elseBranchLabel)] @ 
+    compile' trueBranch @ 
+    [JMP endLabel] @ 
+    [LABEL elseBranchLabel] @ 
+    compile' elseBranch @ 
+    [LABEL endLabel]
+
+  | Stmt.While (cond, body) ->
+    let condLabel = labelGenerator#generateNamed "while_cond" in
+    let loopLabel = labelGenerator#generateNamed "while_body" in
+    let endLabel  = labelGenerator#generateNamed "while_end"  in
+    [LABEL condLabel] @
+    expr cond @
+    [CJMP ("z", endLabel)] @
+    compile' body @
+    [JMP condLabel] @
+    [LABEL endLabel]
+
+  | Stmt.Repeat (body, cond) ->
+    let loopLabel = labelGenerator#generateNamed "repeat" in
+    [LABEL loopLabel] @
+    compile' body @
+    expr cond @
+    [CJMP ("z", loopLabel)]
+  
+  | Stmt.Skip -> []
+
+  | Stmt.Call (fname, args) -> 
+    List.concat (List.map expr (List.rev args)) @
+    [CALL fname]
+
+let compile_function (name, (args, locals, body)) = 
+  let compiled = compile' body in
+  [LABEL name] @
+  [BEGIN (args, locals)] @
+  compiled @
+  [END]
+
+let compile (defs, p) = 
+  let main = compile' p in
+  let funcs = List.fold_left 
+    (fun lst def -> let c = compile_function def in c :: lst) [] defs in
+  [LABEL "name"] @
+  main @
+  [END] @
+  (List.concat funcs)
